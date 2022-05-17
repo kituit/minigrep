84f72b0e74d92967a8b25c10c34e7c28a9ce8b10
Kit Glover <kglover@live.com.au>
2022-05-17 20:19:28 +1000

    Implementation of search and basic unit test

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,11 @@ fn main() {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-    println!("{:?}", args);
-    println!("Searching for {} in file {}", config.query, config.filename);
+    // println!("{:?}", args);
+    // println!("Searching for {} in file {}", config.query, config.filename);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
-
         process::exit(1);
     }
 }
